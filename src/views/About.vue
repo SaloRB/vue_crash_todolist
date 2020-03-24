b65a28741313bbdb5b64bfecedacd69a82c9b3c2
Salomon Romano <salo_romano@yahoo.com>
2020-03-24 00:47:01 -0600

    First Commit

---
+++ src/views/About.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="about">
+    <h1>About</h1>
+    <p>This is the TodoList app v1.0.0. It is part of the Vue crash course on YouTube</p>
+  </div>
+</template>
+
+<style scoped>
+h1 {
+  color: red;
+}
+</style>
